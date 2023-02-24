f5dfeb9ee50c91f8780c4bc8f7d3f6046e1d4186
bagussamito <bagussamito1@gmail.com>
2023-02-24 13:10:03 +0700

    Penjualan Admin

--- lib/app/modules/dashboard_admin/views/dashboard_admin_view.dart
+++ lib/app/modules/dashboard_admin/views/dashboard_admin_view.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:petshop/app/modules/setting/views/setting_view.dart';
 import 'package:petshop/app/theme/theme.dart';
@@ -20,8 +21,8 @@ class DashboardAdminView extends GetView<DashboardAdminController> {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        body: FutureBuilder<DocumentSnapshot<Object?>>(
-            future: controller.getUserDoc(),
+        body: StreamBuilder<DocumentSnapshot<Object?>>(
+            stream: controller.getUserDoc(),
             builder: (context, snap) {
               if (snap.connectionState == ConnectionState.waiting) {
                 return LoadingView();
@@ -54,13 +55,331 @@ class DashboardAdminView extends GetView<DashboardAdminController> {
                           ),
                           Column(
                             children: [
-                              AnimSearchBar(
-                                width: 400,
-                                textController: textController,
-                                onSuffixTap: () {},
-                                color: Yellow1,
-                                onSubmitted: Get.to,
-                              )
+                              SizedBox(
+                                height: bodyHeight * 0.005,
+                              ),
+                              Container(
+                                width: bodyWidth * 1,
+                                height: bodyHeight * 0.085,
+                                child: TextFormField(
+                                  textInputAction: TextInputAction.next,
+                                  autovalidateMode:
+                                      AutovalidateMode.onUserInteraction,
+                                  onTap: () {
+                                    FocusScopeNode currentFocus =
+                                        FocusScope.of(context);
+
+                                    if (!currentFocus.hasPrimaryFocus) {
+                                      currentFocus.unfocus();
+                                    }
+                                  },
+                                  style: TextStyle(color: dark),
+                                  decoration: InputDecoration(
+                                      prefixIcon: Align(
+                                          widthFactor: 1.0,
+                                          heightFactor: 1.0,
+                                          child: FaIcon(
+                                            FontAwesomeIcons.searchengin,
+                                            color: Red1,
+                                          )),
+                                      hintText: 'Cari Barang',
+                                      hintStyle: heading6.copyWith(
+                                          color: Grey1,
+                                          fontSize: 14 * textScale),
+                                      focusColor: Blue1,
+                                      fillColor: light,
+                                      filled: true,
+                                      errorStyle: TextStyle(
+                                        fontSize: 13.5 * textScale,
+                                        color: light,
+                                        background: Paint()
+                                          ..strokeWidth = 13
+                                          ..color = errorBg
+                                          ..style = PaintingStyle.stroke
+                                          ..strokeJoin = StrokeJoin.round,
+                                      ),
+                                      errorBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: errorBg, width: 1.8),
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          gapPadding: 2),
+                                      focusedErrorBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: error, width: 1.8),
+                                          borderRadius:
+                                              BorderRadius.circular(12)),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Blue1, width: 1.8),
+                                          borderRadius:
+                                              BorderRadius.circular(12)),
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12))),
+                                ),
+                              ),
+                              SizedBox(
+                                height: bodyHeight * 0.008,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                  top: bodyHeight * 0.01,
+                                  left: bodyWidth * 0.05,
+                                  right: bodyWidth * 0.05,
+                                ),
+                                width: bodyWidth * 0.8,
+                                height: bodyHeight * 0.40,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.circular(
+                                              10), // Image border
+                                          child: SizedBox.fromSize(
+                                            // Image radius
+                                            child: Image.asset(
+                                              'assets/images/foto1.jpg',
+                                              height: 250,
+                                              width: 270,
+                                              fit: BoxFit.fitHeight,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: bodyHeight * 0.01,
+                                        ),
+                                        Text(
+                                          'Ini Adalah Kucing',
+                                          style: TextStyle(
+                                              fontSize: 20, color: Purple),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          FontAwesomeIcons.paw,
+                                          color: Red1,
+                                        ),
+                                        SizedBox(
+                                          width: bodyWidth * 0.02,
+                                        ),
+                                        Text(
+                                          'Harga Kucing Ini',
+                                          style: TextStyle(
+                                              fontSize: 20, color: Purple),
+                                        ),
+                                        ClipOval(
+                                          child: Material(
+                                            color: Colors.transparent,
+                                            child: IconButton(
+                                              onPressed: () => () {},
+                                              icon: FaIcon(
+                                                FontAwesomeIcons.pencil,
+                                                color: Red1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        ClipOval(
+                                          child: Material(
+                                            color: Colors.transparent,
+                                            child: IconButton(
+                                              onPressed: () => () {},
+                                              icon: FaIcon(
+                                                FontAwesomeIcons.trash,
+                                                color: Red1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                width: bodyHeight * 1,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                  top: bodyHeight * 0.01,
+                                  left: bodyWidth * 0.05,
+                                  right: bodyWidth * 0.05,
+                                ),
+                                width: bodyWidth * 0.8,
+                                height: bodyHeight * 0.40,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.circular(
+                                              10), // Image border
+                                          child: SizedBox.fromSize(
+                                            // Image radius
+                                            child: Image.asset(
+                                              'assets/images/foto1.jpg',
+                                              height: 250,
+                                              width: 270,
+                                              fit: BoxFit.fitHeight,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: bodyHeight * 0.01,
+                                        ),
+                                        Text(
+                                          'Ini Adalah Kucing',
+                                          style: TextStyle(
+                                              fontSize: 20, color: Purple),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          FontAwesomeIcons.paw,
+                                          color: Red1,
+                                        ),
+                                        SizedBox(
+                                          width: bodyWidth * 0.02,
+                                        ),
+                                        Text(
+                                          'Harga Kucing Ini',
+                                          style: TextStyle(
+                                              fontSize: 20, color: Purple),
+                                        ),
+                                        ClipOval(
+                                          child: Material(
+                                            color: Colors.transparent,
+                                            child: IconButton(
+                                              onPressed: () => () {},
+                                              icon: FaIcon(
+                                                FontAwesomeIcons.pencil,
+                                                color: Red1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        ClipOval(
+                                          child: Material(
+                                            color: Colors.transparent,
+                                            child: IconButton(
+                                              onPressed: () => () {},
+                                              icon: FaIcon(
+                                                FontAwesomeIcons.trash,
+                                                color: Red1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                width: bodyHeight * 1,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                  top: bodyHeight * 0.01,
+                                  left: bodyWidth * 0.05,
+                                  right: bodyWidth * 0.05,
+                                ),
+                                width: bodyWidth * 0.8,
+                                height: bodyHeight * 0.40,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.circular(
+                                              10), // Image border
+                                          child: SizedBox.fromSize(
+                                            // Image radius
+                                            child: Image.asset(
+                                              'assets/images/foto1.jpg',
+                                              height: 250,
+                                              width: 270,
+                                              fit: BoxFit.fitHeight,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: bodyHeight * 0.01,
+                                        ),
+                                        Text(
+                                          'Ini Adalah Kucing',
+                                          style: TextStyle(
+                                              fontSize: 20, color: Purple),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          FontAwesomeIcons.paw,
+                                          color: Red1,
+                                        ),
+                                        SizedBox(
+                                          width: bodyWidth * 0.02,
+                                        ),
+                                        Text(
+                                          'Harga Kucing Ini',
+                                          style: TextStyle(
+                                              fontSize: 20, color: Purple),
+                                        ),
+                                        ClipOval(
+                                          child: Material(
+                                            color: Colors.transparent,
+                                            child: IconButton(
+                                              onPressed: () => () {},
+                                              icon: FaIcon(
+                                                FontAwesomeIcons.pencil,
+                                                color: Red1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        ClipOval(
+                                          child: Material(
+                                            color: Colors.transparent,
+                                            child: IconButton(
+                                              onPressed: () => () {},
+                                              icon: FaIcon(
+                                                FontAwesomeIcons.trash,
+                                                color: Red1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
                             ],
                           )
                         ],
